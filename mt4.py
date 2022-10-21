8f7994b82d4880faeda572c0fe4ad4e0d40fb043
minhle211 <91536710+minhle211@users.noreply.github.com>
2022-10-21 21:02:12 +0700

    Add files via upload

---
+++ mt4.py
@@ -0,0 +1,34 @@
+# char['Gold'] += 50
+# print("Gold: ",char['Gold'])
+# char['Backpack']='Shield, Bread Loaf, FlintStone'
+
+# print("Backpack: ",char['Backpack'])
+# char = {
+#     'Name': 'Light',
+#     'Age':17,
+#     'Strength':8,
+#     'Defense':10,
+#     'HP':100,
+#     'Backpack': 'Shield, Bread Loaf',
+#     'Gold': 100,
+#     'Level': 2
+# }
+# skill = {
+#    '1' : {'Name': 'Tackle', 'Minimum level': 1, 'Damage': 5, 'Hit rate': 0.3},
+#    '2' : {'Name': 'Quick Attack', 'Minimum level': 2, 'Damage': 3, 'Hit rate': 0.5},
+#    '3' : {'Name': 'Strong Kick', 'Minimum level': 4, 'Damage': 9, 'Hit rate': 0.2},
+# }
+
+# print("Cho phep: ")
+# for x in skill:
+#     print(f"Skill {x}: {skill[x]['Name']}")
+# inp = input("Choose skill by number: ")
+
+# print(f"You chose {skill[inp]['Name']}.")
+# if(char['Level']>=skill[inp]['Minimum level']):
+#     print(f"Damage: {skill[inp]['Damage']}")
+# else:
+#     print(f"Cannot deploy. Required level: {skill[inp]['Minimum level']}")
+
+
+
